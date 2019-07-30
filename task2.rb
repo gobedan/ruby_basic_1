a5e72beb6098e4182f7452e1ac5b840bc76579b1
Valentin Markin <jobedano@gmail.com>
2019-07-30 15:13:40 +0400

    task2

---
+++ task2.rb
@@ -0,0 +1,6 @@
+# Triangle square 
+print "Triangle base? "
+base = Integer(gets.chomp).abs
+print "Triangle height? "
+height = Integer(gets.chomp).abs
+puts "Square of triangle (base:#{base}; height:#{height}) = #{(base*height)/2}" 
